c0709dd9f209abe2148db046d2241eeec41c1d99
Mihail Chilyashev <chilyashev@gmail.com>
2013-08-24 02:57:54 +0300

    Added link to the post

--- wpc21/main.rb
+++ wpc21/main.rb
@@ -1,16 +1,20 @@
+# http://olimex.wordpress.com/2013/08/23/weekend-programming-challenge-issue-21-words-puzzle/
+# Challenge:
+# Write code which takes as input normal text and shuffles the letters inside the words to make text like this one above.
+
+
 # Takes a word and shuffles its letters keeping the first and the last in their place
 # Parameters:
 # word - The word to shuffle
 #
 # Returns the shuffled word
-
 def shuffle(word)
-  if word.length < 3
+  if word.length <= 3
     return word
   end
   first_letter = word[0]
   last_letter = word[-1]
-  rest = word[1,word.length-2]
+  rest = word[1,word.length-1]
   shuffle = rest.split(//).shuffle.join()
   first_letter + shuffle + last_letter
 end
